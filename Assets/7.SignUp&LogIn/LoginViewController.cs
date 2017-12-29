3e9887fe5af3b387cbb95e98dd8adef5bc0c6188
t_kkoding <hansam2396@gmail.com>
2017-12-29 16:23:13 +0900

    Add LoginViewController

---
+++ Assets/7.SignUp&LogIn/LoginViewController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginViewController : ViewController {
+
+	[SerializeField] private InputField	idInput;
+	[SerializeField] private InputField pwInput;
+	[SerializeField] private Toggle autoLogin;
+	[SerializeField] private Toggle autoSetId;
+	[SerializeField] private Button sigInBtn;
+	[SerializeField] private Button signUpBtn;
+	[SerializeField] private GameObject loadingObj;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
